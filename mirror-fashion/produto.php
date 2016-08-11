062181913d44f7394fe519389c5606436d9e58b4
ljbmoura <ljbmoura@gmail.com>
2016-08-11 15:43:58 -0300

    capitulo11-exercicio 11.9

--- mirror-fashion/produto.php
+++ mirror-fashion/produto.php
@@ -12,41 +12,46 @@
 				<legend>Escolha a cor:</legend>
 
 				<input type="radio" name="cor" value="verde" id="verde" checked>
-				<label for="verde"><img src="img/produtos/foto2-verde.png" alt="verde"></label>
-
-				<input type="radio" name="cor" value="rosa" id="rosa" >
-				<label for="rosa"><img src="img/produtos/foto2-rosa.png" alt="rosa"></label>
-
-				<input type="radio" name="cor" value="azul" id="azul" >
-				<label for="azul"><img src="img/produtos/foto2-azul.png" alt="azul"></label>
+				<label for="verde"><img src="img/produtos/foto2-verde.png"
+					alt="verde"></label> <input type="radio" name="cor" value="rosa"
+					id="rosa"> <label for="rosa"><img
+					src="img/produtos/foto2-rosa.png" alt="rosa"></label> <input
+					type="radio" name="cor" value="azul" id="azul"> <label
+					for="azul"><img src="img/produtos/foto2-azul.png"
+					alt="azul"></label>
 			</fieldset>
 
 			<fieldset class="tamanhos">
 				<legend>Escolha o tamanho:</legend>
-				<input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
-			</fieldset>	
-			<input type="submit" class="comprar" value="Comprar">
-			<input type="hidden" name="nome" value="Fuzzy Cardigan">
-			<input type="hidden" name="preco" value="129.00">
-			<input type="hidden" name="id" value="2">
+				<input type="range" min="36" max="46" value="42" step="2"
+					name="tamanho" id="tamanho">
+				<output for="tamanho" name="valortamanho" class="tamanhos">42</output>
+			</fieldset>
+			<input type="submit" class="comprar" value="Comprar"> <input
+				type="hidden" name="nome" value="Fuzzy Cardigan"> <input
+				type="hidden" name="preco" value="129.00"> <input
+				type="hidden" name="id" value="2">
 		</form>
 	</div>
 	<div class="detalhes">
 		<h2>Detalhes do Produto</h2>
-		<p>Esse é o melhor casaco de Cardigã que você já viu. Excelente material italiano com estampa desenhada pelos artesãos da comunidade de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa entrega ajato.</p>
-	
+		<p>Esse é o melhor casaco de Cardigã que você já viu. Excelente
+			material italiano com estampa desenhada pelos artesãos da comunidade
+			de Krotor nas ilhas gregas. Compre já e receba hoje mesmo pela nossa
+			entrega ajato.</p>
+
 		<table>
 			<thead>
 				<tr>
-				<th>Características</th>
-				<th>Detalhes</th>
+					<th>Características</th>
+					<th>Detalhes</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
 					<td>Modelo</td>
 					<td>Cardigã 7845</td>
-				</tr>	
+				</tr>
 				<tr>
 					<td>Material</td>
 					<td>Algodáo e Poliester</td>
@@ -61,8 +66,8 @@
 				</tr>
 			</tbody>
 		</table>
-	</div>	
+	</div>
 </div>
-
+<script type="text/javascript" src="js/produto.js"></script>
 <?php
 	include("rodape.php"); ?>
