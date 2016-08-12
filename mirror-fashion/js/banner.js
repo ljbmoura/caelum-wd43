3e395bfc34fb7d7f2bf5d09c0317616774330d7b
ljbmoura <ljbmoura@gmail.com>
2016-08-12 11:01:01 -0300

    final capitulo 11

--- mirror-fashion/js/banner.js
+++ mirror-fashion/js/banner.js
@@ -2,13 +2,31 @@
  * 
  */
 
-var banners = ["img/destaque-home.png","img/destaque-home-2.png"];
+var banners = [ "img/destaque-home.png", "img/destaque-home-2.png",
+		"img/destaque_home_3.png" ];
 
 var img_banner = document.querySelector(".destaque img");
+var banner_atual = 0;
+
+var btn_pausa = document.querySelector("#pausa_play");
 
 function trocaBanner() {
-	banners.reverse();
-	img_banner.src = banners[0];
+	// banners.reverse();
+	banner_atual = (banner_atual + 1) % (banners.length);
+	img_banner.src = banners[banner_atual];
 }
 
-setInterval(trocaBanner, 2000);
+var timer = setInterval(trocaBanner, 2000);
+
+btn_pausa.addEventListener("click", function() {
+//	setTimeout(callback, delay)
+//	btn_pausa.caption = "Play";
+	if(btn_pausa.innerHTML == "Pausa"){
+		btn_pausa.innerHTML="Play";
+		clearInterval(timer);
+	} else {
+		btn_pausa.innerHTML="Pausa";
+		timer = setInterval(trocaBanner, 2000)
+	}
+}
+)
